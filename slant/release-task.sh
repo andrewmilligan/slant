b09d0d7bd51b9b0a08f9090a89222be75470db49
andrewmilligan <andrew.i.milligan@gmail.com>
2017-11-03 11:22:48 -0600

    tweaked the release task to not have to be executed from that directory

--- slant/release-task.sh
+++ slant/release-task.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 # RELOAD THE PORTFOLIO PROJECTS
-python manage.py loadportfolio
+python "$DIR/manage.py" loadportfolio
 if [[ $? -ne 0 ]]; then
   echo "Failure loading portfolio stories."
   exit 1
 fi
 
 # RELOAD THE SLANT STORIES
-python manage.py dvloadstories
+python "$DIR/manage.py" dvloadstories
 if [[ $? -ne 0 ]]; then
   echo "Failure loading dataviz stories."
   exit 1
